546be7077d707e7ae4fab732a90f406bb5ad988e
HaydenSchilling <42759774+HaydenSchilling@users.noreply.github.com>
2019-12-04 16:56:10 +1100

    Updated Map Figure

--- map figure.R
+++ map figure.R
@@ -30,10 +30,10 @@ head(Aus_coast)
 
 #plot(Aus_coast)
 
-min_lon <- 149
-max_lon <- 155
-min_lat <- -36
-max_lat <- -32
+min_lon <- 150.3
+max_lon <- 152.8
+min_lat <- -35.5
+max_lat <- -32.8
 
 geo_bounds <- c(left = min_lon, bottom = min_lat, right = max_lon, top = max_lat)
 
@@ -53,17 +53,17 @@ shapes = "1"
 
 p1 <- ggplot() + theme_classic() + 
   labs(x=expression(paste("Longitude (",degree, ")", sep="")), y=expression(paste("Latitude (", degree, ")"))) +
-  scale_x_continuous(expand = c(0,0), limits = c(149,153)) + scale_y_continuous(expand = c(0,0), limits = c(-36,-32)) +
+  scale_x_continuous(expand = c(0,0), limits = c(150.3,152.5)) + scale_y_continuous(expand = c(0,0), limits = c(-35.5,-32.8)) +
   #scale_fill_distiller(palette = "Spectral", na.value=NA, trans="log10") +
   coord_quickmap() + #coord_map() + #  # this line could be very slow
   geom_polygon(data = Aus_crop, aes(x=long, y = lat, group = group), fill = "gray60", colour = "gray60")+
   geom_polygon(data = estuaries, aes(x=long, y = lat, group = group), fill = "white", colour = "white")+
   geom_rect(data = boxes, aes(xmin=x_max, xmax=x_min, ymin=y_min, ymax=y_max, group = Estuary), fill = NA, col = "black") +
-  theme(axis.title.x = element_text(face="bold", colour="black", size = 18),
-        axis.text  = element_text(colour="black", size = 12), 
-        axis.title.y = element_text(face="bold", colour="black", size = 18),
+  theme(axis.title.x = element_text(face="bold", colour="black", size = 16),
+        axis.text  = element_text(colour="black", size = 10), 
+        axis.title.y = element_text(face="bold", colour="black", size = 16),
         axis.ticks = element_line(colour="black"),
-        strip.text = element_text(colour="black", face = "bold", size = 14),
+        strip.text = element_text(colour="black", face = "bold", size = 12),
         strip.background = element_rect(colour = NULL),
         legend.justification=c(1,0), legend.position=c(0.92,0.05), legend.direction = "horizontal",
         panel.border = element_rect(colour = "black", fill=NA, size = 1),
@@ -71,16 +71,22 @@ p1 <- ggplot() + theme_classic() +
         #legend.text = element_text(size = 12, face = "bold"),
         legend.title=element_blank(),
         legend.background = element_blank())  +
+  scalebar(NULL, x.min =150.3, x.max = 152.5, y.min = -35.5, y.max = -32.8,
+           dist = 30, dist_unit = "km",
+           transform = T, model = "WGS84", st.bottom = F,
+           anchor = c("x" = 152.3,"y"= -35.48)) +
+  north(scale = 0.1, symbol = 3, anchor = c("x" = 152.9,"y"= -32.05),
+               x.min =150.3, x.max = 152.5, y.min = -35.5, y.max = -32.8)+
   #geom_point(data = dots, aes(x = Longitude, y = Latitude*-1, col = cols), size = 2) +
   #scale_shape_manual(values = 1, label ="Larval Fish \nSample") +
   #geom_point(data = NULL, aes(x = 151.258693, y = 33.838509*-1), size = 2, col = "blue") +
   geom_text(data = boxes, aes(x = x_min, y = y_min, label = Estuary), 
-            nudge_x = c(0.75,0.55,0.8))# +
+            nudge_x = c(0.5,0.45,0.65))# +
 
 p1
 
-ggsave("plots/Figure 1.pdf")
-ggsave("plots/Figure 1.png", dpi = 600)
+#ggsave("plots/Figure 1.pdf")
+#ggsave("plots/Figure 1.png", dpi = 600)
 
 
 ### Online code for inset
@@ -101,7 +107,7 @@ library(gridExtra)
 # munnames$label<-mrdq@data$NAME_2
 
 # Extent rectangle for inset map
-pol<-data.frame(xmin=149,xmax=153 ,ymin=-36 ,ymax=-32)
+pol<-data.frame(xmin=150,xmax=153 ,ymin=-36 ,ymax=-32)
 
 
 # Main Map
@@ -118,7 +124,7 @@ p2<-ggplot()+geom_polygon(data=Aus_coast, aes(x=long, y = lat, group = group), f
   coord_quickmap()+theme_bw()+labs(x=NULL,y=NULL) + 
   #scale_x_continuous(breaks=seq(117.5,125, 2.5), labels=c(paste(seq(117.5,125, 2.5),"Â°E", sep="")))+
   #scale_y_continuous(breaks=seq(5,20, 5), labels=c(paste(seq(5,20, 5),"Â°N", sep="")))+
-  geom_rect(data = pol, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), alpha=0, colour="red", size = 1, linetype=1)+
+  geom_rect(data = pol, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), alpha=0, colour="red", size = 0.7, linetype=1)+
   theme(axis.text.x =element_blank(),axis.text.y= element_blank(), axis.ticks=element_blank(),axis.title.x =element_blank(),
         axis.title.y= element_blank(), plot.background = element_rect(fill = "transparent", color = NA)
   ) 
@@ -129,11 +135,18 @@ p2
 png(file="Figure 1.png",w=1800,h=1800, res=300)
 grid.newpage()
 v1<-viewport(width = 1, height = 1, x = 0.5, y = 0.5) #plot area for the main map
-v2<-viewport(width = 0.3, height = 0.35, x = 0.32, y = 0.855, clip = "off") #plot area for the inset map
+v2<-viewport(width = 0.2, height = 0.25, x = 0.348, y = 0.903, clip = "off") #plot area for the inset map
 print(p1,vp=v1) 
 print(p2,vp=v2)
 dev.off()
 
+pdf(file="Figure 1.pdf",w=7,h=7)
+grid.newpage()
+v1<-viewport(width = 1, height = 1, x = 0.5, y = 0.5) #plot area for the main map
+v2<-viewport(width = 0.2, height = 0.25, x = 0.33, y = 0.903, clip = "off") #plot area for the inset map
+print(p1,vp=v1) 
+print(p2,vp=v2)
+dev.off()
 
 
 ### Now to make the individual estuary maps
@@ -154,29 +167,32 @@ pBB <- ggplot(Bb) + theme_classic() +
   geom_polygon(aes(x=long, y = lat, group = group), fill = "white", colour = "white")+
   geom_rect(data = boxes, aes(xmin=x_max, xmax=x_min, ymin=y_min, ymax=y_max, group = Estuary), fill = NA, col = "black") +
   #facet_wrap(~Estuary)+
-  theme(axis.title.x = element_text(face="bold", colour="black", size = 18),
-        axis.text  = element_text(colour="black", size = 12), 
-        axis.title.y = element_text(face="bold", colour="black", size = 18),
+  theme(axis.title.x = element_blank(),
+        axis.text  = element_text(colour="black", size = 5), 
+        axis.title.y = element_blank(),
         axis.ticks = element_line(colour="black"),
-        strip.text = element_text(colour="black", face = "bold", size = 14),
-        strip.background = element_rect(colour = NULL),
-        legend.justification=c(1,0), legend.position="right", legend.direction = "vertical",
-        panel.border = element_rect(colour = "black", fill=NA, size = 1),
+        #strip.text = element_text(colour="black", face = "bold", size = 14),
+        #strip.background = element_rect(colour = NULL),
+        legend.justification=c(1,0), legend.position="none", legend.direction = "vertical",
+        #panel.border = element_rect(colour = "black", fill=NA, size = 1),
         #legend.key.size = unit(1, "cm"),
-        #legend.text = element_text(size = 12, face = "bold"),
-        legend.title=element_blank(),
-        legend.background = element_blank())  +
+        legend.text = element_text(size = 3, face = "bold"),
+        legend.title=element_text(colour="black", face = "bold", size = 4),
+        legend.background = element_rect(colour = "black", fill="white", size = 0.2)) +
+  scale_shape_manual(values = c(0,1))+
   scalebar(NULL, x.min =boxes$x_min[3], x.max = boxes$x_max[3], y.min = boxes$y_min[3],
            y.max = boxes$y_max[3], dist = 1, dist_unit = "km",
-           transform = T, model = "WGS84", st.bottom = FALSE,
-           anchor = c("x" = 151.2,"y"= -34.03)) +
+           transform = T, model = "WGS84", st.bottom = T,
+           anchor = c("x" = 151.247,"y"= -33.973), border.size = 0.5, st.size = 1.5) +
   #geom_point(data = dots, aes(x = Longitude, y = Latitude*-1, col = cols), size = 2) +
   #scale_shape_manual(values = 1, label ="Larval Fish \nSample") +
-  geom_point(data = Bb_reefs, aes(x = Long, y = Lat, shape = Type), size = 1.5) #+
+  geom_point(data = Bb_reefs, aes(x = Long, y = Lat, shape = Type), size = 1) #+
   #geom_text(data = boxes, aes(x = x_min, y = y_min, label = Estuary), 
   #          nudge_x = c(0.75,0.55,0.8))# +
 
 pBB
+
+ggsave("Botany Bay map.pdf", height = 6, width = 7, units = "cm")
 
 
 # Lake Macquarie
@@ -193,30 +209,32 @@ pLM <- ggplot(LM) + theme_classic() +
   geom_polygon(aes(x=long, y = lat, group = group), fill = "white", colour = "white")+
   geom_rect(data = boxes, aes(xmin=x_max, xmax=x_min, ymin=y_min, ymax=y_max, group = Estuary), fill = NA, col = "black") +
   #facet_wrap(~Estuary)+
-  theme(axis.title.x = element_text(face="bold", colour="black", size = 18),
-        axis.text  = element_text(colour="black", size = 12), 
-        axis.title.y = element_text(face="bold", colour="black", size = 18),
+  theme(axis.title.x = element_blank(),
+        axis.text  = element_text(colour="black", size = 5), 
+        axis.title.y = element_blank(),
         axis.ticks = element_line(colour="black"),
-        strip.text = element_text(colour="black", face = "bold", size = 14),
-        strip.background = element_rect(colour = NULL),
-        legend.justification=c(1,0), legend.position="right", legend.direction = "vertical",
-        panel.border = element_rect(colour = "black", fill=NA, size = 1),
+        #strip.text = element_text(colour="black", face = "bold", size = 14),
+        #strip.background = element_rect(colour = NULL),
+        legend.justification=c(1,0), legend.position="none", legend.direction = "vertical",
+        #panel.border = element_rect(colour = "black", fill=NA, size = 1),
         #legend.key.size = unit(1, "cm"),
-        #legend.text = element_text(size = 12, face = "bold"),
-        legend.title=element_blank(),
-        legend.background = element_blank())  +
+        legend.text = element_text(size = 3, face = "bold"),
+        legend.title=element_text(colour="black", face = "bold", size = 4),
+        legend.background = element_rect(colour = "black", fill="white", size = 0.2))  +
   scalebar(NULL, x.min =boxes$x_min[3], x.max = boxes$x_max[3], y.min = boxes$y_min[3],
            y.max = boxes$y_max[3], dist = 1, dist_unit = "km",
            transform = T, model = "WGS84", st.bottom = FALSE,
-           anchor = c("x" = 151.65,"y"= -33.14)) +
+           anchor = c("x" = 151.67,"y"= -33.118), border.size = 0.5, st.size = 1.5) +
+  scale_shape_manual(values = c(0,1)) +
   #geom_point(data = dots, aes(x = Longitude, y = Latitude*-1, col = cols), size = 2) +
   #scale_shape_manual(values = 1, label ="Larval Fish \nSample") +
-  geom_point(data = LM_reefs, aes(x = Long, y = Lat, col = Type), size = 1.5) #+
+  geom_point(data = LM_reefs, aes(x = Long, y = Lat, shape = Type), size = 1) #+
 #geom_text(data = boxes, aes(x = x_min, y = y_min, label = Estuary), 
 #          nudge_x = c(0.75,0.55,0.8))# +
 
 pLM
 
+ggsave("Lake Macquarie Map.pdf", height = 6, width = 7, units = "cm")
 
 
 # St Georges Basin
@@ -233,26 +251,29 @@ pSGB <- ggplot(SBG) + theme_classic() +
   geom_polygon(aes(x=long, y = lat, group = group), fill = "white", colour = "white")+
   geom_rect(data = boxes, aes(xmin=x_max, xmax=x_min, ymin=y_min, ymax=y_max, group = Estuary), fill = NA, col = "black") +
   #facet_wrap(~Estuary)+
-  theme(axis.title.x = element_text(face="bold", colour="black", size = 18),
-        axis.text  = element_text(colour="black", size = 12), 
-        axis.title.y = element_text(face="bold", colour="black", size = 18),
+  theme(axis.title.x = element_blank(),
+        axis.text  = element_text(colour="black", size = 5), 
+        axis.title.y = element_blank(),
         axis.ticks = element_line(colour="black"),
-        strip.text = element_text(colour="black", face = "bold", size = 14),
-        strip.background = element_rect(colour = NULL),
-        legend.justification=c(1,0), legend.position="right", legend.direction = "vertical",
-        panel.border = element_rect(colour = "black", fill=NA, size = 1),
+        #strip.text = element_text(colour="black", face = "bold", size = 14),
+        #strip.background = element_rect(colour = NULL),
+        legend.justification=c(1,0), legend.position=c(0.255, 0.0), legend.direction = "vertical",
+        #panel.border = element_rect(colour = "black", fill=NA, size = 1),
         #legend.key.size = unit(1, "cm"),
-        #legend.text = element_text(size = 12, face = "bold"),
-        legend.title=element_blank(),
-        legend.background = element_blank())  +
+        legend.text = element_text(size = 3, face = "bold"),
+        legend.title=element_text(colour="black", face = "bold", size = 4),
+        legend.background = element_rect(colour = "black", fill="white", size = 0.2))  +
+  scale_shape_manual(values = c(0,1), name = "Reef Type") +
   scalebar(NULL, x.min =boxes$x_min[3], x.max = boxes$x_max[3], y.min = boxes$y_min[3],
           y.max = boxes$y_max[3], dist = 1, dist_unit = "km",
           transform = T, model = "WGS84", st.bottom = FALSE,
-          anchor = c("x" = 150.64,"y"= -35.18)) +
+          anchor = c("x" = 150.66,"y"= -35.185), border.size = 0.5, st.size = 1.5) +
   #geom_point(data = dots, aes(x = Longitude, y = Latitude*-1, col = cols), size = 2) +
   #scale_shape_manual(values = 1, label ="Larval Fish \nSample") +
-  geom_point(data = SBG_reefs, aes(x = Long, y = Lat, shape = Type), size = 1.5) #+
+  geom_point(data = SBG_reefs, aes(x = Long, y = Lat, shape = Type), size = 1) #+
 #geom_text(data = boxes, aes(x = x_min, y = y_min, label = Estuary), 
 #          nudge_x = c(0.75,0.55,0.8))# +
 
 pSGB
+
+ggsave("St Georges Basin Map.pdf", height = 6, width = 7, units = "cm")
